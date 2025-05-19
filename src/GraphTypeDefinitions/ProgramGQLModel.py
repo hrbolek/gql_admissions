ef8d557a6cef433b9f7a7881926591a978bb8670
Hrbolek <alexandr.stefek@unob.cz>
2025-05-19 21:29:02 +0200

    improvements in functionality

--- src/GraphTypeDefinitions/ProgramGQLModel.py
+++ src/GraphTypeDefinitions/ProgramGQLModel.py
@@ -1,13 +1,15 @@
+import typing
 import strawberry
 import uuid
 
 import strawberry.types
 
-from uoishelpers.gqlpermissions import RBACObjectGQLModel
-
+from uoishelpers.gqlpermissions import RBACObjectGQLModel, OnlyForAuthentized
+from uoishelpers.resolvers import VectorResolver
 # from .BaseGQLModel import _BaseGQLModel as BaseGQLModel
 from .BaseGQLModel import BaseGQLModel, BaseGQLModelEx
 
+from .AdmissionGQLModel import AdmissionGQLModel, AdmissionInputFilter
 @strawberry.federation.type(keys=["id"], extend=True) #, description="study program (accreditation)")
 class ProgramGQLModel(BaseGQLModelEx, BaseGQLModel):
     from .BaseGQLModel import (
@@ -21,3 +23,12 @@ class ProgramGQLModel(BaseGQLModelEx, BaseGQLModel):
         rbacobject_id,
         rbacobject
     )
+
+    admissions: typing.List["AdmissionGQLModel"] = strawberry.field(
+        description="Admissions for this program",
+        permission_classes=[
+            OnlyForAuthentized
+        ],
+        resolver=VectorResolver["AdmissionGQLModel"](fkey_field_name="program_id", whereType=AdmissionInputFilter),
+    )
+    
